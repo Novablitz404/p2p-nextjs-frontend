02009b6667dcd9dcb8ce9024093398079b89e6e7
OvLabsTechnology <ovlabstechnology@gmail.com>
2025-06-26 22:10:31 +0800

    fix: reverted

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // This is a more comprehensive list to ensure all dependencies are handled
-  transpilePackages: [
-    '@walletconnect/ethereum-provider',
-    '@web3modal/standalone',
-    'viem',
-    '@wagmi/core',
-    '@wagmi/connectors',
-    '@tanstack/query-core',
-    '@tanstack/react-query',
-    '@coinbase/wallet-sdk',
-    'eventemitter3'
-  ],
   images: {
     remotePatterns: [
       {
